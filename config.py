b977922cea30d091f7a29526eb278d6aa2da4ee7
LePhucThanh <115394175+uwuvnxakuh@users.noreply.github.com>
2022-11-17 17:14:13 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+tokenapp="d09ad780663211edb73b39415ecf8faa"
+android="31"
+device="Samsung SM-A225F"
+locale="VN"
+deviceToken="f5gZ6preS2yFlShLZ2OLEA:APA91bGjMqn8qNacnTXno1ckM0GkTEupqCmqvHCnhZ2MXdCnwWVUAKmLO0pw5lS8gzUB-twbAjBNDz_rrkPtujTJefdXfmCcvZ27Ch9wBXh8jHos9oxlGTG0GpiXWcuuZNnFqPoincTz"
+
+# Phần này trở xuống dành cho bản trả phí, các bạn có thể bỏ qua nhe
+proxies = {
+	'http': '',
+    'https': ''
+}
+username_proxy = ''
+password_proxy = ''
